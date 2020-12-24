8e904a16e389b09004dea522fa8e159df81fbb0d
yguilai <1007139643@qq.com>
2020-12-24 16:58:00 +0800

    jwt rpc: fulfil create jwt token logic

--- services/jwt/rpc/internal/logic/create_logic.go
+++ services/jwt/rpc/internal/logic/create_logic.go
@@ -2,10 +2,12 @@ package logic
 
 import (
 	"context"
+	"time"
 
 	"github/yguilai/timetable-micro/services/jwt/rpc/internal/svc"
 	"github/yguilai/timetable-micro/services/jwt/rpc/jwt"
 
+	jwtGo "github.com/dgrijalva/jwt-go"
 	"github.com/tal-tech/go-zero/core/logx"
 )
 
@@ -24,7 +26,31 @@ func NewCreateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CreateLogi
 }
 
 func (l *CreateLogic) Create(in *jwt.JwtCreateReq) (*jwt.JwtCreateResp, error) {
-	// todo: add your logic here and delete this line
+	ac := l.svcCtx.Config.Auth
 
-	return &jwt.JwtCreateResp{}, nil
+	now := time.Now().Unix()
+	token, err := createJwtToken(now, ac.AccessExpire, ac.AccessSecret, in.Claims)
+	if err != nil {
+		return nil, err
+	}
+	return &jwt.JwtCreateResp{
+		Token: &jwt.Token{
+			AccessToken:  token,
+			AccessExpire: now + ac.AccessExpire,
+			RefreshAfter: now + ac.AccessExpire/2,
+		},
+	}, nil
+}
+
+func createJwtToken(iat, expire int64, secret string, payloads map[string]string) (string, error) {
+	claims := make(jwtGo.MapClaims)
+	claims["exp"] = iat + expire
+	claims["iat"] = iat
+
+	for k, v := range payloads {
+		claims[k] = v
+	}
+	token := jwtGo.New(jwtGo.SigningMethodHS256)
+	token.Claims = claims
+	return token.SignedString([]byte(secret))
 }
--- services/jwt/rpc/internal/logic/ping_logic.go
+++ services/jwt/rpc/internal/logic/ping_logic.go
@@ -24,7 +24,5 @@ func NewPingLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PingLogic {
 }
 
 func (l *PingLogic) Ping(in *jwt.Request) (*jwt.Response, error) {
-	// todo: add your logic here and delete this line
-
-	return &jwt.Response{}, nil
+	return &jwt.Response{Pong: "pong: " + in.Ping}, nil
 }
