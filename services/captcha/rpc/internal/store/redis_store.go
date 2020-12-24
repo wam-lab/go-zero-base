35d3815e0d1be9ca307284746aacc843b1fefe6e
yguilai <1007139643@qq.com>
2020-12-24 09:53:19 +0800

    captcha service: add redis store for base64Captcha

---
+++ services/captcha/rpc/internal/store/redis_store.go
@@ -0,0 +1,58 @@
+package store
+
+import (
+	"fmt"
+	"github.com/tal-tech/go-zero/core/logx"
+	"github.com/tal-tech/go-zero/core/stores/redis"
+	"strings"
+)
+
+type RedisStore struct {
+	redis *redis.Redis
+}
+
+func NewRedisStore(redis *redis.Redis) *RedisStore {
+	return &RedisStore{redis: redis}
+}
+
+func (s *RedisStore) Set(id string, value string) {
+	err := s.redis.Set(id, value)
+	if err != nil {
+		logx.Errorf(fmt.Sprintf("RedisStore set %s:%s failed, err: %v", id, value, err))
+		return
+	}
+}
+
+func (s *RedisStore) Get(id string, clear bool) string {
+	val, err := s.redis.Get(id)
+	if err != nil {
+		logx.Errorf(fmt.Sprintf("RedisStore get %s failed, err: %v", id, err))
+		return val
+	}
+
+	if clear {
+		go func() {
+			// TODO: need use redis lock?
+			//lock := redis.NewRedisLock(s.redis, id)
+			//lock.SetExpire(1)
+			//_, _ = lock.Acquire()
+			//defer func() {
+			//	released, err := lock.Release()
+			//	if err != nil || !released {
+			//		logx.Errorf(fmt.Sprintf("Release Lock %s failed, err: %v", id, err))
+			//	}
+			//}()
+			_, err := s.redis.Del(id)
+			if err != nil {
+				logx.Errorf(fmt.Sprintf("RedisStore del %s failed, err: %v", id, err))
+				return
+			}
+		}()
+	}
+	return val
+}
+
+func (s *RedisStore) Verify(id, answer string, clear bool) bool {
+	ansInRedis := strings.TrimSpace(s.Get(id, clear))
+	return ansInRedis == answer
+}
